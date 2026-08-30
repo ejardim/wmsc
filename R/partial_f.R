370dacc758f43ad1517bf064e869b6ff3621f42a
danaim <mantopoulou.danai@gmail.com>
2026-08-30 15:57:31 +0200

    updated partial F

--- R/partial_f.R
+++ R/partial_f.R
@@ -1,10 +1,10 @@
 #' partial_f calculates fishing mortality by fleet
 #'
-#' Produces a FLQuant with fishing mortalities by unit.
+#' Produces a FLStocks with the harvest slot update
 #'
 #' @param fit An object of class a4aFitSA with the assessment fit (no default)
 #' @param partial_stocks An object of class FLStocks containing the partial stock objects (fleets)
-#' @return FLQuants
+#' @return FLStocks
 #' @export
 
 
@@ -15,10 +15,15 @@ partial_f <- function(fit, partial_stocks, ...) {
   spread(list(...))
   
   f_tot <- harvest(fit)
-  c_fleets <- catch.n(partial_stocks)
+  c_fleets <- lapply(partial_stocks, catch.n)
   c_tot <- Reduce("+", c_fleets)
   pf <- lapply(c_fleets, function(x) {
     (x / c_tot) * f_tot
   })
-  return(pf)
+
+ upd_stks <- FLStocks(Map(function(x, y) {
+  harvest(x) <- y  
+  return(x) 
+  }, partial_stocks, pf))
+  return(upd_stks)
 }
